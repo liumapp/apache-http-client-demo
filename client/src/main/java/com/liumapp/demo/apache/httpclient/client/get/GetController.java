292f3fbecbe69baf55cdb8771670990dbc62235a
liumapp <liumapp.com@gmail.com>
2018-03-02 16:22:14 +0800

    send get request to google done

--- client/src/main/java/com/liumapp/demo/apache/httpclient/client/get/GetController.java
+++ client/src/main/java/com/liumapp/demo/apache/httpclient/client/get/GetController.java
@@ -1,7 +1,16 @@
 package com.liumapp.demo.apache.httpclient.client.get;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by liumapp on 3/2/18.
@@ -11,4 +20,42 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("get")
 public class GetController {
+
+    @RequestMapping("/toGoogle")
+    public void sendGetRequestToGoogle () {
+
+        try {
+            String url = "http://www.google.com/search?q=httpClient";
+
+            HttpClient client = HttpClientBuilder.create().build();
+            HttpGet request = new HttpGet(url);
+
+            // add request header
+            request.addHeader("User-Agent", "Mozila/5.0");
+            HttpResponse response = client.execute(request);
+
+            System.out.println("Response Code : "
+                    + response.getStatusLine().getStatusCode());
+
+            BufferedReader rd = new BufferedReader(
+                    new InputStreamReader(response.getEntity().getContent()));
+
+            StringBuffer result = new StringBuffer();
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @RequestMapping("/toLocal")
+    public void sendGetRequestToLocal () {
+
+    }
+
 }
